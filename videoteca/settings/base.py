74312104225410581a0ea2d63da8766aead255e1
jofaco <jofaco1916@hotmail.com>
2023-11-01 10:05:32 -0500

    Update base.py

--- videoteca/settings/base.py
+++ videoteca/settings/base.py
@@ -168,7 +168,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
