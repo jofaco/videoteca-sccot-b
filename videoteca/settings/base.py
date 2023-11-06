684e8e21e9f1efbf77dd9300b769f93fe7fb5f30
jofaco <jofaco1916@hotmail.com>
2023-11-06 12:12:15 -0500

    Se elimina acceso en cors a cualquier dominio

--- videoteca/settings/base.py
+++ videoteca/settings/base.py
@@ -141,7 +141,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "*"
+    "http://165.227.177.75:8000",
+    "http://165.227.177.75:8080",
+    "http://165.227.177.75",
+    "http://videos.sccot.org",
 ]
 
 CORS_ORIGIN_WHITELIST = [
@@ -149,7 +152,10 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "*"
+    "http://165.227.177.75:8000",
+    "http://165.227.177.75:8080",
+    "http://165.227.177.75",
+    "http://videos.sccot.org",
 ]
 
 SIMPLE_JWT = {
